0f4ea3e4503de85aec817ef4ca685ef27b73f738
Jack wu <jackwu833016@gmail.com>
2017-12-03 17:42:57 -0500

    add netbean project folder format

--- ParisMetro.java
+++ ParisMetro.java
@@ -1,47 +1,52 @@
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import net.datastructures.ArrayList;
+import java.util.ArrayList;
 import java.io.IOException;
+import java.util.*;
 
 @SuppressWarnings("unchecked")
 public class ParisMetro {
 
 		private String tempName;
-		private String[] tempNames;
+        private String[] tempNames;
 		
 		public ParisMetro (String fileName) throws Exception, IOException {
-			read(fileName); //Allows one to read the .txt file
+			getStopInfoArray(fileName); //Allows one to read the .txt file
 		}
-		
-		protected void read(String fileName) throws Exception, IOException {
-			BufferedReader mapFile = new BufferedReader(new FileReader(fileName));
-		}
-		
-		public static void main (String[] args){
+        
+        /**
+         * @return StringArray
+         */
+		protected ArrayList<String> getStopInfoArray(String fileName) throws IOException {
 
-            ArrayList<String> stopName = new ArrayList(); //Store everything in ArrayList
-            String name = "";
+            ArrayList<String> stopNameList = new ArrayList<String>(376);                   
+            String readInLine = "";
 
             try{
 
-                BufferedReader stationName = new BufferedReader(new FileReader("docs/metro.txt"));
+                BufferedReader stationName = new BufferedReader(new FileReader(fileName));
                 stationName.readLine(); //Skips first line of .txt
                 
-                while (name != "$") //Iterates til the "$"
+                while (readInLine != "$") //Iterates til the "$"
                 {
-                    name = stationName.readLine(); //String reading
-                    String[] tempNames = name.split("\\s"); //Split the String, store into array of strings called tempNames[]	
-                    stopName.add(stopName.size(), (tempNames[1])); //Adds the 2nd half of the string into the arraylist. For example, if text is 0000 hello, then only "hello" is stored in the ArrayList
+                    readInLine = stationName.readLine(); //String reading
+
+                    String stopName = readInLine.split("\\s")[1];
+                    stopNameList.add(stopName); //Adds the 2nd half of the string into the arraylist. For example, if text is 0000 hello, then only "hello" is stored in the ArrayList
                     
-                    System.out.println(tempNames[0] + ", " + tempNames[1]);
                 }
 
             }catch (IOException exception){
                 System.out.println("Error occur: " + exception.toString());
             }
 
-            return;
+            return stopNameList;
+		}
+		
+		public static void main (String[] args){
+            ParisMetro PM = new ParisMetro();
 
+            PM.getStopInfoArray(args.toString());
 		}
 }
